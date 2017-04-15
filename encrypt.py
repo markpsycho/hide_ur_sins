24592110b124e32e187ebf5b2a3a4ecef22fdd66
markpsycho <markpsycho1@gmail.com>
2017-04-15 22:30:28 +0530

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,40 @@
+import base64
+import sys
+import random
+# print ("enter: filename d|en corruptionword[greater than 3] outputfile ")
+# www=sys.stdin.readline()
+# args=www.split()
+
+try:
+    arg=sys.argv[2]#args[1]        #input("encode/decode:")
+    filename =sys.argv[1]#args[0]  #input("input file:")
+    filename2 =sys.argv[4]#args[3] #input("output file")
+    dummyname =sys.argv[3]#args[2] #input("enter dummy name")
+    if (( arg !='d') & (arg != 'en')) :
+        print ("ERROR:\nenter: filename d|en corruptionword[greater than 3] outputfile ")
+        sys.exit()
+except Exception as e:
+        print ('ERROR: requires 4 arguements: ')
+        print ("enter: filename d|en corruptionword[greater than 3] outputfile ")
+        sys.exit()
+
+try:
+        fileobject=open(filename,"rb")
+        fileobject1=open(filename2,"wb")
+except Exception as e:
+        print ('Did I jump directly to here?' + str(e))
+        sys.exit()
+
+data=fileobject.read()
+if(arg=='en'):
+    encoded_data = base64.b64encode(data)
+    x=random.randint(len(encoded_data)/2,len(encoded_data))
+    random_encoded=encoded_data[:x]+bytes(dummyname, 'utf-8')+encoded_data[x:]
+elif(arg=='d'):
+    data1=data.replace(bytes(dummyname, 'utf-8'),bytes('', 'utf-8'))
+    encoded_data = base64.b64decode(data1)
+    random_encoded=encoded_data
+
+fileobject1.write(random_encoded)
+fileobject1.close()
+fileobject.close()
